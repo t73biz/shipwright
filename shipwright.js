4e641893d0a47bf346d4dfa8469ef4ba8b919b25
t73biz <rchaplin@t73.biz>
2013-07-18 12:27:00 -0700

    Implementing logic for CLI

--- shipwright.js
+++ shipwright.js
@@ -1,4 +1,40 @@
 #!/usr/bin/env node
-var argv = require('optimist')
-	.usage('Usage: $0 ')
+var usageHelp = require('./lib/help/usage');
+var builder   = require('./lib/util/builder');
+var designer  = require('./lib/util/designer');
+var parser    = require('./lib/util/parser');
+var argv      = require('optimist')
+	.usage(usageHelp.content)
+	.options(
+		'c',
+		{
+        	alias    : 'config',
+	        describe : 'The configuration file to use for building'
+    	}
+    )
+    .demand('c')
+    .check(
+    	function(argv)
+    	{
+    		var checkArc = argv._.indexOf('design');
+    		var checkBld = argv._.indexOf('build');
+    		if(checkArc == -1 && checkBld == -1)
+    		{
+   				return false;
+    		}
+			return true;
+    	}
+    )
 	.argv;
+
+switch(argv._[0])
+{
+	case "design":
+		designer.design();
+	break;
+
+	case "build":
+		configuration = parser.parse(argv.c);
+		builder.build(configuration);
+	break;
+}
